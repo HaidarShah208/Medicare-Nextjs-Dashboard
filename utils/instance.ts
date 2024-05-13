6f2f86807b66b96bcfc51d24d436553ae0cc20b3
HaidarShah208 <alhi7896542@gmail.com>
2024-05-13 16:19:56 +0500

    feat:set image alt

--- utils/instance.ts
+++ utils/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://medicare-nextjs-dashboard-zyx.vercel.app/api/",
+  baseURL: "http://localhost:3000/api/",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
